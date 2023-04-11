6485558a3ca9594faa67e61b197819558dd823d2
DarkS34 <masthaker@gmail.com>
2023-04-11 21:47:44 +0200

    act.: - Versión final

---
+++ components/home/home_template.js
@@ -0,0 +1,21 @@
+export const homeTemplate = () => `
+<section id="home">
+  <div class="introduction">
+    <span class="intro-text">Hola, mi nombre es</span>
+    <span class="name">OLEKSANDR YATSENTYUK</span>
+    <span class="profesion">y soy FullStack Developer.</span>
+  </div>
+  <div class="details-card">
+    <p>
+      Soy un estudiante de ingeniería informática, apasionado por la
+      programación y en constante búsqueda de la perfección en mi trabajo.
+      Me encanta la idea de crear aplicaciones y sitios web útiles para
+      las personas, y siempre estoy en constante actualización y
+      aprendizaje de las últimas tendencias y herramientas en el mercado.
+      Si necesitas un desarrollador comprometido y apasionado, ¡no dudes
+      en contactarme!
+    </p>
+    <img class="intro-emoji" src="https://res.cloudinary.com/datpjjoam/image/upload/v1681241287/Rock%7BtheCode%7D/Projects/Project_3_Portfolio/memoji.png">
+  </div>
+</section>
+`;
--- main.js
+++ main.js
@@ -1,19 +1,176 @@
-import { jobExperience } from "./experiences";
-import { projects } from "./projects";
 import "./style.css";
+import { homeTemplate } from "./components/home/home_template";
+import { jobExperience } from "./components/experience/experience_constants";
 import {
-  homeTemplate,
-  projectTemplate,
   experienceContainerTemplate,
   experienceTemplate,
-} from "./templates";
+} from "./components/experience/experience_templates";
+import { projects } from "./components/projects/project_constants";
+import { projectTemplate } from "./components/projects/projects-templates";
 
+const sectionLinks = document.querySelectorAll(".link");
 const menuButton = document.querySelector(".menu-btn");
+const menuLinks = document.querySelectorAll(".site-menu a");
 const mainContainer = document.querySelector(".site-main");
 const footerContainer = document.querySelector(".site-footer");
+let currentSection = "home";
 
 const mainContainsSlide = () => {
   return mainContainer.classList.contains("slide-mode");
+};
+
+const changeSelectedSection = (section) => {
+  sectionLinks.forEach((e) => {
+    if (
+      e.innerText === section &&
+      e.parentElement.classList.contains("navigation-item")
+    ) {
+      e.parentElement.style.filter = "drop-shadow(0 0 10px #fff)";
+      e.parentElement.style.opacity = "1";
+      e.parentElement.style.transform = "translateY(-1px)";
+      e.style.borderBottom = "1px solid";
+    } else if (
+      e.innerText !== section &&
+      e.parentElement.classList.contains("navigation-item")
+    ) {
+      e.parentElement.style.filter = "";
+      e.parentElement.style.opacity = "";
+      e.parentElement.style.transform = "";
+      e.style.borderBottom = "";
+    }
+    if (
+      e.innerText === section &&
+      e.parentElement.classList.contains("navigation-item-menu")
+    ) {
+      e.parentElement.style.marginInlineStart = "1rem";
+      e.parentElement.style.opacity = "1";
+      e.parentElement.style.filter = "drop-shadow(0 0 10px rgb(29, 29, 30))";
+      e.innerHTML = "▸ " + e.innerHTML;
+    } else if (
+      e.innerText !== section &&
+      e.parentElement.classList.contains("navigation-item-menu")
+    ) {
+      e.parentElement.style.marginInlineStart = "";
+      e.parentElement.style.opacity = "";
+      e.parentElement.style.filter = "";
+      e.innerText = e.innerText.split("").some((e) => e === "▸")
+        ? e.innerText.split("").slice(2).join("")
+        : e.innerText;
+    }
+  });
+};
+
+const renderHome = () => {
+  mainContainer.innerHTML += homeTemplate();
+  changeSelectedSection("HOME");
+};
+
+const renderExperience = () => {
+  mainContainer.innerHTML += experienceContainerTemplate();
+  const experienceContainer = document.querySelector(".experience-container");
+  jobExperience.forEach((e) => {
+    experienceContainer.innerHTML += experienceTemplate(
+      e.company,
+      e.logoSvg,
+      e.dates,
+      e.description,
+      e.responsibilities
+    );
+  });
+
+  const cardsContainer = document.querySelector(".experience-container");
+  const expCards = document.querySelectorAll(".experience-card");
+  const rightBtn = document.querySelectorAll(".btn-right");
+  const leftBtn = document.querySelectorAll(".btn-left");
+  let midCardPos = 0;
+
+  expCards[midCardPos].classList.toggle("middle-card");
+  expCards[midCardPos + 1].classList.add("turn-right");
+
+  const setCardClasses = (mCardPos) => {
+    expCards.forEach((e) =>
+      e.classList.remove("middle-card", "turn-left", "turn-right")
+    );
+    expCards[mCardPos].classList.add("middle-card");
+    if (mCardPos > 0) {
+      expCards[mCardPos - 1].classList.add("turn-left");
+    }
+    if (mCardPos < expCards.length - 1) {
+      expCards[mCardPos + 1].classList.add("turn-right");
+    }
+  };
+
+  const scrollBymediaQuery = () => {
+    if (window.matchMedia("(min-width: 1025px)").matches) {
+      return 257;
+    } else if (
+      window.matchMedia("(min-width: 769px) and (max-width: 1024px)").matches
+    ) {
+      return 202;
+    } else if (
+      window.matchMedia("(min-width: 481px) and (max-width: 768px)").matches
+    ) {
+      return 102;
+    } else if (window.matchMedia("(max-width: 480px)").matches) {
+      return 108;
+    }
+  };
+
+  const btnsDisable = () => {
+    midCardPos === 0
+      ? leftBtn.forEach((e) => (e.style.opacity = ".1"))
+      : leftBtn.forEach((e) => (e.style.opacity = "1"));
+    midCardPos === expCards.length - 1
+      ? rightBtn.forEach((e) => (e.style.opacity = ".1"))
+      : rightBtn.forEach((e) => (e.style.opacity = "1"));
+  };
+
+  btnsDisable();
+
+  rightBtn.forEach((e) => {
+    e.addEventListener("click", () => {
+      if (midCardPos !== expCards.length - 1) {
+        cardsContainer.scroll({
+          left: cardsContainer.scrollLeft + scrollBymediaQuery(),
+          behavior: "smooth",
+        });
+        setCardClasses(++midCardPos);
+        btnsDisable();
+      }
+    });
+  });
+
+  leftBtn.forEach((e) => {
+    e.addEventListener("click", () => {
+      if (midCardPos !== 0) {
+        cardsContainer.scroll({
+          left: cardsContainer.scrollLeft - scrollBymediaQuery(),
+          behavior: "smooth",
+        });
+        setCardClasses(--midCardPos);
+        btnsDisable();
+      }
+    });
+  });
+
+  cardsContainer.addEventListener("click", (ev) => {
+    const expArticle = ev.target.closest("article");
+    expCards.forEach((e, i) => {
+      if (e === expArticle && midCardPos !== i) {
+        if (expArticle) {
+          expCards.forEach((e) => e.classList.remove("middle-card"));
+          expArticle.classList.toggle("middle-card");
+        }
+        midCardPos = i;
+        cardsContainer.scrollTo({
+          left: i * scrollBymediaQuery(),
+          behavior: "smooth",
+        });
+        setCardClasses(midCardPos);
+        btnsDisable();
+      }
+    });
+  });
 };
 
 const renderProjects = () => {
@@ -51,124 +208,13 @@ const renderProjects = () => {
   document.querySelectorAll(".project").forEach((el) => observer.observe(el));
 };
 
-const renderExperience = () => {
-  const setCardClasses = (mCardPos) => {
-    const expCards = document.querySelectorAll(".experience-card");
-    expCards.forEach((e) =>
-      e.classList.remove("middle-card", "turn-left", "turn-right")
-    );
-    expCards[mCardPos].classList.add("middle-card");
-    if (mCardPos > 0) {
-      expCards[mCardPos - 1].classList.add("turn-left");
-    }
-    if (mCardPos < expCards.length - 1) {
-      expCards[mCardPos + 1].classList.add("turn-right");
-    }
-  };
-
-  mainContainer.innerHTML += experienceContainerTemplate();
-  const experienceContainer = document.querySelector(".experience-container");
-  jobExperience.forEach((e) => {
-    experienceContainer.innerHTML += experienceTemplate(
-      e.company,
-      e.logoSvg,
-      e.dates,
-      e.description,
-      e.responsibilities
-    );
-  });
-
-  const cardsContainer = document.querySelector(".experience-container");
-  const expCards = document.querySelectorAll(".experience-card");
-  let midCardPos = 0;
-  const rightBtn = document.querySelectorAll(".btn-right");
-  const leftBtn = document.querySelectorAll(".btn-left");
-
-  expCards.forEach((e, i) => {
-    if (i !== midCardPos) {
-      e.classList.add("turn-right");
-    }
-  });
-  expCards[midCardPos].classList.toggle("middle-card");
-
-  const scrollBymediaQuery = () => {
-    if (window.matchMedia("(min-width: 1025px)").matches) {
-      return 257;
-    } else if (
-      window.matchMedia("(min-width: 769px) and (max-width: 1024px)").matches
-    ) {
-      return 202;
-    } else if (
-      window.matchMedia("(min-width: 481px) and (max-width: 768px)").matches
-    ) {
-      return 102;
-    } else if (window.matchMedia("(max-width: 480px)").matches) {
-      return 108;
-    }
-  };
-
-  const btnsDisable = () => {
-    midCardPos === 0
-      ? leftBtn.forEach((e) => (e.style.opacity = ".1"))
-      : leftBtn.forEach((e) => (e.style.opacity = "1"));
-    midCardPos === expCards.length - 1
-      ? rightBtn.forEach((e) => (e.style.opacity = ".1"))
-      : rightBtn.forEach((e) => (e.style.opacity = "1"));
-  };
-
-  btnsDisable();
-  rightBtn.forEach((e) => {
-    e.addEventListener("click", () => {
-      if (midCardPos !== expCards.length - 1) {
-        cardsContainer.scroll({
-          left: cardsContainer.scrollLeft + scrollBymediaQuery(),
-          behavior: "smooth",
-        });
-        setCardClasses(++midCardPos);
-        btnsDisable();
-      }
-    });
-  });
-
-  leftBtn.forEach((e) => {
-    e.addEventListener("click", () => {
-      if (midCardPos !== 0) {
-        cardsContainer.scroll({
-          left: cardsContainer.scrollLeft - scrollBymediaQuery(),
-          behavior: "smooth",
-        });
-        setCardClasses(--midCardPos);
-        btnsDisable();
-      }
-    });
-  });
-
-  cardsContainer.addEventListener("mousedown", (ev) => {
-    const expArticle = ev.target.closest("article");
-    expCards.forEach((e, i) => {
-      if (e === expArticle && midCardPos !== i) {
-        if (expArticle) {
-          expCards.forEach((e) => e.classList.remove("middle-card"));
-          expArticle.classList.toggle("middle-card");
-        }
-        midCardPos = i;
-        cardsContainer.scrollTo({
-          left: i * scrollBymediaQuery(),
-          behavior: "smooth",
-        });
-        setCardClasses(midCardPos);
-        btnsDisable();
-      }
-    });
-  });
-};
-
-document.querySelectorAll(".link").forEach((link) =>
+sectionLinks.forEach((link) =>
   link.addEventListener("click", (ev) => {
     ev.preventDefault();
     const category = ev.target.pathname.slice(1);
 
-    if (!mainContainsSlide()) {
+    if (!mainContainsSlide() && currentSection !== category) {
+      currentSection = category;
       mainContainer.classList.toggle("slide-mode");
       footerContainer.classList.toggle("slide-mode");
       setTimeout(() => {
@@ -177,13 +223,16 @@ document.querySelectorAll(".link").forEach((link) =>
         mainContainer.innerHTML = "";
         switch (category) {
           case "home":
-            mainContainer.innerHTML += homeTemplate();
+            renderHome();
+            changeSelectedSection("HOME");
             break;
           case "experience":
             renderExperience();
+            changeSelectedSection("EXPERIENCIA");
             break;
           case "projects":
             renderProjects();
+            changeSelectedSection("PROYECTOS");
             break;
         }
       }, 1100);
@@ -191,18 +240,16 @@ document.querySelectorAll(".link").forEach((link) =>
   })
 );
 
-document.querySelectorAll(".site-menu a").forEach((link) => {
-  link.addEventListener("click", (ev) => {
-    menuButton.click();
-  });
-});
-
 menuButton.addEventListener("click", () => {
   menuButton.classList.toggle("menu-mode");
   document.querySelector(".site-menu").classList.toggle("menu-mode");
   document.querySelector(".logo").classList.toggle("menu-mode");
 });
 
-// Se carga la página de home
-// mainContainer.innerHTML += homeTemplate();
-renderExperience();
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (ev) => {
+    menuButton.click();
+  });
+});
+
+renderHome();
