48b9418f35a496c01867393bdb3bf00fcd794b05
DarkS34 <masthaker@gmail.com>
2025-07-31 23:59:51 +0200

    rearanged about-me section

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -3,24 +3,22 @@ import "./AboutMe.css";
 const AboutMe = () => {
   return (
     <>
-      <section id="home">
-        <div class="introduction">
-          <span class="intro-text">Hi, my name is</span>
-          <span class="name">OLEKSANDR YATSENTYUK</span>
-          <span class="profesion">and I'm a Computer Engineer</span>
-        </div>
-        <div class="details-card">
-          <p>
+      <section id="about-me">
+        <div class="presentation">
+          <span>Hi, my name is</span>
+          <span>OLEKSANDR YATSENTYUK</span>
+          <span>and I'm a Computer Engineer</span>
+          <p class="presentation-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
             inventore quos fugiat dolor sequi eaque eum eos ullam? Ratione
             quibusdam voluptatem cupiditate necessitatibus pariatur repellendus
             culpa iusto minus, praesentium quisquam!
           </p>
-          <img
-            class="intro-emoji"
-            src="https://res.cloudinary.com/projectsfs/image/upload/v1753738871/Rock%7BtheCode%7D/Projects/Project_3_Portfolio/foto_carne_nb_wr2rk8.png"
-          />
         </div>
+        <img
+          src="https://res.cloudinary.com/projectsfs/image/upload/v1753738871/Rock%7BtheCode%7D/Projects/Project_3_Portfolio/foto_carne_nb_wr2rk8.png"
+          alt="personal-photo"
+        />
       </section>
     </>
   );
